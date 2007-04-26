6620b2362d2b5280a8edd8290e2afa923c1acc08
wonder_mice <wonder_mice@191519d4-f22e-0410-bfe0-ca2722524e22>
2007-04-26 06:30:37 +0000

    initial

---
+++ trunk/xdt/bit_box/bit_box.h
@@ -0,0 +1,95 @@
+
+
+
+//! \brief Class contains types and constants of bit_box container. It also
+//! has some basic functionality.
+class basic_bit_box {
+public:
+	// public types ------------------------------------------------------------
+
+	typedef unsigned int sz_t;
+	typedef unsigned char byte_t;
+	typedef unsigned int type_t;
+
+	//! \brief Item header of each bit_box data
+	#pragma pack(push, 2)
+	struct item_t {
+		sz_t sz;
+		type_t type;
+	};
+	#pragma pack(pop)
+
+	// public constants --------------------------------------------------------
+
+	//! \brief Predefined types for bit_box data
+	enum bb_types {
+		bbt_bit_box,	//!< \brief Data is another bit_box
+		bbt_binary,		//!< \brief Data is byte array
+		bbt_int32,		//!< \brief Data is 32 bit signed integer
+		bbt_uint32,		//!< \brief Data is 32 bit unsigned integer
+		bbt_double,		//!< \brief Data is 64 bit floating point value
+		bbt_string,		//!< \brief Data is null-terminated ASCII string
+		bbt_wstring,	//!< \brief Data is null-terminated UTF-16 string
+		bbt_user = 0xFFF	//! \brief This and greater for user defined types
+	};
+
+	// public methods ----------------------------------------------------------
+
+	//! \brief Constructor
+
+protected:
+	// protected methods -------------------------------------------------------
+
+	//! \brief 
+	void _set(const byte_t *const bits) {
+	}
+
+	//! \brief Constructor
+	void _set(const byte_t *const bits, const sz_t sz) {
+		_bits	= bits;
+		_sz		= sz;
+	}
+
+private:
+	const byte_t *_bits;
+	sz_t _sz;
+};
+
+
+//! \brief Class that can iterate over bit_box data
+class reader: public basic_bit_box {
+public:
+	// public types ------------------------------------------------------------
+
+	//! \brief Constant iterator over bit_box data
+	class const_iterator {
+	};
+
+	// public methods ----------------------------------------------------------
+
+	//! \brief Copy constructor
+	/*!	Copy constructor of reader class does not copies any memory, just bits
+		pointer and size value.
+	*/
+	reader(const reader &r);
+
+	//! \brief Constructor
+	/*!	Takes bits like it is bit_box item, so size can be taken from bit_box
+		item header. Type in header must be basic_bit_box::bbt_bit_box. Class
+		remembers just size and pointer to data itself, not on entire item.
+	*/
+	reader(const byte_t *const bits);
+
+	//! \brief Constructor
+	/*! 
+	*/
+	reader(const byte_t *const bits, const sz_t sz);
+
+protected:
+private:
+
+};
+
+
+class composer {
+};
