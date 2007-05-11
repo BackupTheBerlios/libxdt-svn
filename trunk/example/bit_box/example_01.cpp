08f9e45918122f18d45eb5b534608a35d43f207b
wonder_mice <wonder_mice@191519d4-f22e-0410-bfe0-ca2722524e22>
2007-05-11 15:38:45 +0000

    bit_box.h small changes

--- trunk/example/bit_box/example_01.cpp
+++ trunk/example/bit_box/example_01.cpp
@@ -1,13 +1,11 @@
 #include <xdt/bit_box/basic_bit_box.h>
+#include <xdt/bit_box/bit_box.h>
 
 int main() {
-	xdt::bit_box::basic_bit_box<char> c1(0);
-	xdt::bit_box::basic_bit_box<const char> c2(0);
+	xdt::bit_box::bit_box<xdt::byte_t> bb(0);
+	xdt::bit_box::bit_box<xdt::byte_t>::iterator i(0);
 
-	xdt::bit_box::basic_bit_box<char> c3(c1);
-	xdt::bit_box::basic_bit_box<const char> c4(c1);
-	xdt::bit_box::basic_bit_box<char> c5(c2);
-	xdt::bit_box::basic_bit_box<const char> c6(c2);
+	xdt::bit_box::bit_box<const xdt::byte_t>::iterator j = bb.begin();
 
 	return -1;
 }
--- trunk/xdt/bit_box/bit_box.h
+++ trunk/xdt/bit_box/bit_box.h
@@ -44,7 +44,8 @@ namespace bit_box
 
 ////////////////////////////////////////////////////////////////////////////////
 // bit_box class declaration
-//! \brief
+//! \brief Elementary bit_box container that can read and iterate throw
+//!	bit_box data.
 /*!	
 */
 template <class data_type>
@@ -52,10 +53,31 @@ class bit_box: public basic_bit_box<data_type>
 {
 public:
 	// public types ------------------------------------------------------------
-	class const_iterator: public basic_bit_box
+
+	//!	\brief 
+	/*!	
+	*/
+	class iterator: public basic_bit_box<data_type>
 	{
 	public:
-		bool operator ==(const_iterator &i2);
+		// public methods ------------------------------------------------------
+
+		//! \brief Constructor
+		/*!	
+		*/
+		iterator(data_type *const bits): basic_bit_box<data_type>(bits)
+		{
+		}
+
+		template <class t>
+		iterator(typename const bit_box<t>::iterator &i):
+			basic_bit_box<data_type>(i)
+		{
+		}
+
+		bool operator ==(const iterator &b) {
+			return (b.bits() == b.bits());
+		}
 	};
 
 	// public methods ----------------------------------------------------------
@@ -63,19 +85,19 @@ public:
 	//!	\brief Constructor
 	/*!	\param[in] bits Pointer on bit_box data with header
 	*/
-	bit_box(const byte_t *const bits): basic_bit_box(bits)
+	bit_box(byte_t *const bits): basic_bit_box<data_type>(bits)
 	{
 	}
 
-	const_iterator begin() const;
-	const_iterator end() const;
+	iterator begin() const
+	{
+		return iterator(bits());
+
+	}
+
+	iterator end() const;
 
 protected:
-	// protected types ---------------------------------------------------------
-	class _basic_iterator: public basic_bit_box
-	{
-		bool operator ==(const_iterator &i2);
-	};
 
 	// protected methods -------------------------------------------------------
 
